2235ab01d9574e4526a21cfbce445f278121e4f8
IBeaconNotify <anhquan310399@gmail.com>
2020-07-01 16:15:52 +0700

    commit

--- IBeaconNotify/AppDelegate.swift
+++ IBeaconNotify/AppDelegate.swift
@@ -7,31 +7,51 @@
 //
 
 import UIKit
+import CoreLocation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    let locationManager=CLLocationManager()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        locationManager.delegate=self
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert,.sound]){(granted,error) in}
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+}
 
-
+extension AppDelegate:CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        print("helllo")
+        guard region is CLBeaconRegion else {
+            print("hello 2")
+            return
+        }
+        
+        let content = UNMutableNotificationContent()
+        content.title = "IBeacon Notify"
+        content.body = "Thong bao Ibeacon"
+        content.sound = .defaultCritical
+        
+        let request=UNNotificationRequest(identifier: "IBeaconNotify", content: content, trigger: nil)
+        UNUserNotificationCenter.current().add(request,withCompletionHandler: nil)
+    }
 }
 
